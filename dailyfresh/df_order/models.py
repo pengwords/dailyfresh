6ca4f43fcda785c7943340abca26768ad45e8911
peng <475177575@qq.com>
2019-05-26 16:23:52 +0800

    order

---
+++ dailyfresh/df_order/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Order_Info(models.Model):#主表
+    oid =models.CharField(max_length=20,primary_key=True)#订单编号，主键
+    user =models.ForeignKey('df_user.UserInfo')#用户
+    odate = models.DateTimeField(auto_now=True)#创建时间
+    oIspay = models.BooleanField(default=False)#是否支付
+    ototal = models.DecimalField(max_digits=6,decimal_places=2)#总金额
+    #max_digits表示正数最大位数，decomal_places表示小数位的位数
+class Order_DetailInfo(models.Model):
+    goods = models.ForeignKey('df_goods.GoodsInfo')
+    order = models.ForeignKey(Order_Info)
+    price = models.DecimalField(max_digits=5,decimal_places=2)
+    count = models.IntegerField()
