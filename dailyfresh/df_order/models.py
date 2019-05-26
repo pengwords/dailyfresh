f9e9b28932f6b5911b42a7986ef3c9b80f65ccbe
peng <475177575@qq.com>
2019-05-26 16:26:30 +0800

    order

--- dailyfresh/df_order/models.py
+++ dailyfresh/df_order/models.py
@@ -7,9 +7,9 @@ class Order_Info(models.Model):#主表
     odate = models.DateTimeField(auto_now=True)#创建时间
     oIspay = models.BooleanField(default=False)#是否支付
     ototal = models.DecimalField(max_digits=6,decimal_places=2)#总金额
-    #max_digits表示正数最大位数，decomal_places表示小数位的位数
+    #max_digits表示所有位数的最大位数，decomal_places表示小数位的位数
 class Order_DetailInfo(models.Model):
     goods = models.ForeignKey('df_goods.GoodsInfo')
     order = models.ForeignKey(Order_Info)
     price = models.DecimalField(max_digits=5,decimal_places=2)
-    count = models.IntegerField()
+    count = models.IntegerField()#数量
