d6037a14d8ebf4abcdbafd0195d7e0bfffdcbccb
peng <475177575@qq.com>
2019-05-27 13:26:06 +0800

    订单

--- dailyfresh/df_order/models.py
+++ dailyfresh/df_order/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Order_Info(models.Model):#主表
-    oid =models.CharField(max_length=20,primary_key=True)#订单编号，主键
-    user =models.ForeignKey('df_user.UserInfo')#用户
-    odate = models.DateTimeField(auto_now=True)#创建时间
-    oIspay = models.BooleanField(default=False)#是否支付
-    ototal = models.DecimalField(max_digits=6,decimal_places=2)#总金额
-    #max_digits表示所有位数的最大位数，decomal_places表示小数位的位数
+
+
+class Order_Info(models.Model):  # 主表
+    oid = models.CharField(max_length=20, primary_key=True)  # 订单编号，主键
+    user = models.ForeignKey('df_user.UserInfo',on_delete=models.CASCADE)  # 用户
+    odate = models.DateTimeField(auto_now=True)  # 创建时间
+    oIspay = models.BooleanField(default=False)  # 是否支付
+    ototal = models.DecimalField(max_digits=6, decimal_places=2)  # 总金额
+    oaddress=models.CharField(max_length=100)#收货地址
+    # max_digits表示所有位数的最大位数，decomal_places表示小数位的位数
+#无法实现 真实支付，物流信息（要调用对应接口)
+
 class Order_DetailInfo(models.Model):
-    goods = models.ForeignKey('df_goods.GoodsInfo')
-    order = models.ForeignKey(Order_Info)
-    price = models.DecimalField(max_digits=5,decimal_places=2)
-    count = models.IntegerField()#数量
+    goods = models.ForeignKey('df_goods.GoodsInfo',on_delete=models.CASCADE)  # 关联商品表
+    order = models.ForeignKey(Order_Info,on_delete=models.CASCADE)  # 关联订单表
+    price = models.DecimalField(max_digits=5, decimal_places=2)  # 商品价格
+    count = models.IntegerField()  # 商品数量
